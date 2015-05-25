8fabcaced2244505c07b6d9e003f9c7ac0a681f6
CamCairns <cam.cairns1@gmail.com>
2015-05-25 14:50:24 +1000

    Added surface field options
    Code relies on the variables.has_key() function to detect the presence or absense of a pressure variable (see in load_coord_data).
    
    Basically just use a bunch of
    if plev:
        <DO plev-lat stuff>
    else:
        <DO lat stuff>
    
    statements at the relevant sections. Not super elegant but does aviod a lot of uneccesary duplicate code.

--- SPOOKIE-data-interp.py
+++ SPOOKIE-data-interp.py
@@ -15,37 +15,32 @@ for i1, experi in enumerate(experi_list):
                     print experi, freq, realm, vari, model
                     files = get_SPOOKIE_filepath(experi, freq, realm, vari, model, mount)
                     if files:
+                        files = get_SPOOKIE_filepath(experi, freq, realm, vari, model, mount)
                         plev_common = [100000, 92500, 85000, 70000, 60000, 50000, 40000, 30000, 25000, 20000, 15000, 10000, 7000, 5000, 3000, 2000, 1000]
                         latb_common= np.linspace(-90,90,65);
-                        lat_common = 0.5*(3*latb_common[0]-latb_common[1])+np.cumsum(np.diff(latb_common));
+                        lat_common = 0.5*(3*latb[0]-latb[1])+np.cumsum(np.diff(latb));
 
                         time_init= np.empty([1]) # initalise newtemp with the correct dimensions
                         time_units = []
                         time_cal = []
                     
-                        print 'These are the files: \n', files
-                        nc = netcdf_file(files[0])
-                        lat= np.squeeze(nc.variables['lat'][:])
-                        if 'MPI-ESM-LR' == model:
-                            plev= np.squeeze(nc.variables['lev'][:]) # name difference plev, lev
-                        else:
-                            plev= np.squeeze(nc.variables['plev'][:])
-                        nc.close
-                        time= np.squeeze(nc.variables['time'][:])
-
+                        lat, plev = load_coord_data(files)
+                    
                         model_size = find_model_size(files, vari) # find the total time length of the model, use to preallocate a numpy array
                         print 'model_size', model_size
-                
-                        tmp_array = np.empty([model_size,len(plev),len(lat)])*np.nan;
-                        print 'tmp_array', np.shape(tmp_array)
-                
+                    
+                        if plev:
+                            tmp_array = np.empty([model_size,len(plev),len(lat)])*np.nan;
+                        else:
+                            plev_common = None
+                            tmp_array = np.empty([model_size,len(lat)])*np.nan;
+                    
                         #FIELD DATA EXTRACTION AND CONCATENATION    
-                        print "This is my vari", vari
-                        tmp_array = extract_nc_data(files, vari, tmp_array, 1.0e8);
+                        tmp_array = extract_nc_data(files, vari, tmp_array, model_size, 1.0e8);
 
-                        # INTERPOLATE ONTO COMMON GRID
-                        temp_array_interp = interp_data(lat,plev,lat_common,plev_common,tmp_array)                    
-                
+                        #INTERPOLATE ONTO COMMON GRID
+                        tmp_array_interp = interp_data(lat, plev, lat_common, plev_common, tmp_array)                    
+                    
                         # TIME DATA EXTRACTION
                         tmp_array = np.empty([model_size])*np.nan;
                         time_array = extract_nc_time(files, tmp_array)
@@ -53,14 +48,12 @@ for i1, experi in enumerate(experi_list):
                         time_units = np.append(time_units,nc.variables['time'].units)
                         time_cal = np.append(time_cal,nc.variables['time'].calendar)
                         nc.close
-                
-                        # Make directory path
+                    
+                        # Make directory path                    
                         save_path = '/Users/camcairns/' + mount + '/SPOOKIE_interp/' + experi + '/' + freq + '/' + realm + '/' + vari + '/' + model
                         mkdir_p(save_path)
-
                         # Write nc files
                         nc_file = vari + '_' + freq + '_' + experi + '_SPOOKIE_interp.nc'
-                        write_nc(lat_common, latb_common, plev_common, temp_array_interp, time_array, time_units, time_cal, save_path + '/' + nc_file, vari)
-                        print '.nc written for ' + experi + ' ' + freq + ' '+ realm + ' '+ vari + ' '+ model
+                        write_nc(lat_common, latb_common, plev_common, tmp_array_interp, time_array, time_units, time_cal, save_path + '/' + nc_file, vari)
                     else: 
                         pass
--- nc_data.py
+++ nc_data.py
@@ -6,6 +6,24 @@ import glob
 import os
 import errno
 
+def load_coord_data(files):
+    nc = netcdf_file(files[0])
+    if nc.variables.has_key('lat'):
+        lat= np.squeeze(nc.variables['lat'][:])
+
+    if nc.variables.has_key('plev'):
+        plev= np.squeeze(nc.variables['plev'][:]) # name difference plev, lev
+    elif nc.variables.has_key('lev'):
+        plev= np.squeeze(nc.variables['lev'][:])
+    elif nc.variables.has_key('pfull'):
+        plev= np.squeeze(nc.variables['pfull'][:])
+    else:
+        print "treating variable " + vari + " as a surface field"
+        plev = None
+    nc.close
+#     time= np.squeeze(nc.variables['time'][:])
+    return lat, plev
+
 def extract_nc_data(files, nc_vari, tmp_array, error_limit):
     """Extracts data from a bunch of nc files
 
@@ -25,16 +43,15 @@ def extract_nc_data(files, nc_vari, tmp_array, error_limit):
     print 'Number of Files', len(files)
     for k in range(len(files)):
         nc = netcdf_file(files[k],'r')
-        nc_data = np.squeeze(nc.variables[nc_vari][:])
-        nc_data = nc_data.copy() # 
-        nc.close()
-        nc_data[nc_data>error_limit]= np.nan; # I'd like to make this code genreal for any variable, use dictionary here to assign different tolerances, maybe extract it from the netcdf file?
-        nc_data = np.squeeze(np.nanmean(nc_data,3)); # Take a zonal mean, id like to make this more general as well, automatically pick out the lon dimension?
-        print 'nc_data_file size', np.size(nc_data,0)
+        temp = np.squeeze(nc.variables[nc_variable_name][:])
+        temp = temp.copy() # make a copy because temp is a read only version 
+        nc.close
+        temp[temp>error_limit]= np.nan; # I'd like to make this code genreal for any variable, use dictionary here to assign different tolerances, maybe extract it from the netcdf file?
+        temp = np.squeeze(np.nanmean(temp,len(np.shape(temp))-1)); # Take a zonal mean, id like to make this more general as well, automatically pick out the lon dimension?
+        print 'temp_file size', np.size(temp,0)
         print 'model_size', model_size
-        tmp_array[model_size:model_size+np.size(nc_data,0),...] = nc_data
-        model_size = model_size + np.size(nc_data,0)
-
+        tmp_array[model_size:model_size+np.size(temp,0),...] = temp
+        model_size = model_size + np.size(temp,0)
     return tmp_array
     
 def extract_nc_time(files, tmp_array):
@@ -123,18 +140,26 @@ The function uses the griddata function, this allows NaNs, (particularly boundar
     Raises:
     """
 
-    array_interp = np.empty([np.size(array,0), len(plev_new), len(lat_new)])
-    xold, yold = np.meshgrid(lat_old,plev_old);
-    xnew, ynew = np.meshgrid(lat_new,plev_new);
+    if plev_old:
+        array_interp = np.empty([np.size(array,0), len(plev_new), len(lat_new)])
+        xold, yold = np.meshgrid(lat_old,plev_old);
+        xnew, ynew = np.meshgrid(lat_new,plev_new);
+    else:
+        array_interp = np.empty([np.size(array,0), len(lat_new)])
 
     for m in xrange(np.size(array,0)):
-        if np.sum(~np.isnan(array[m,:,:]))==0:  # Some data in the cmip archive is a month of all NaNs, here we set the output as all NaN for that month
-            array_interp[:,:,m] = np.nan
+        if np.sum(~np.isnan(array[m,...]))==0:  # Some data in the cmip archive is a month of all NaNs, here we set the output as all NaN for that month
+            array_interp[m,...] = np.nan
         else:
-            mask = ~np.isnan(array[m,:,:])
-            points =  zip(xold[mask], yold[mask])
-            temp = array[m,:,:]
-            array_interp[m,:,:] = griddata(points, temp[mask],(xnew,ynew),method='cubic')
+            mask = ~np.isnan(array[m,...])
+            if plev_old:
+                points =  zip(xold[mask], yold[mask])
+                temp = array[m,:,:]
+                array_interp[m,:,:] = griddata(points, temp[mask],(xnew,ynew),method='cubic')
+            else:
+                points = lat_old[mask]
+                temp = array[m,:]
+                array_interp[m,:] = griddata(points, temp[mask],lat_new,method='cubic')
 
     return array_interp
         
@@ -157,26 +182,35 @@ def write_nc(input_lat, input_latb, input_plev, input_array,  time_array, time_u
     rootgrp = Dataset(save_path, 'w', format='NETCDF4')
 
     # Set Dimensions
-    plev = rootgrp.createDimension('plev', 17)
+    if input_plev:
+        plev = rootgrp.createDimension('plev', 17)
+    else:
+        pass
     lat = rootgrp.createDimension('lat', 64)
     latb = rootgrp.createDimension('latb', 65)
     time = rootgrp.createDimension('time', None)
 
     # Set Variables
     times = rootgrp.createVariable('time','f8',('time',))
-    plev = rootgrp.createVariable('plev','f8',('plev',))
+    if input_plev:
+        plev = rootgrp.createVariable('plev','f8',('plev',))
+        tmp = rootgrp.createVariable(nc_vari,'f8',('time','plev','lat'))
+    else:
+        tmp = rootgrp.createVariable(nc_vari,'f8',('time','lat'))
     latitudes = rootgrp.createVariable('latitude','f8',('lat',))
     latitude_bnds = rootgrp.createVariable('latitude_bnds','f8',('latb',))
     # two dimensions unlimited.
-    tmp = rootgrp.createVariable(nc_vari,'f8',('time','plev','lat'))
 
     # Attributes for nc file
     import time
-    rootgrp.description = 'SPOOKIE data with a zonal mean taken and then interpolated on to a uniform lat-lev grid using cubic splines. This file has been written in NETCDF4 format using Pythons netcdf4 library. This nc_files is for MODEL: ' + model + '  FREQ: ' + freq + '  EXPERIMENT: ' + experi + '  REALM: ' + realm + '  VARI: ' + vari
-    rootgrp.history = 'Created ' + time.strftime('%X %x %Z') + '. Original data sourced from DKRZ EUCLIPSE sever.'
+    rootgrp.description = 'SPOOKIE data with a zonal mean taken and then interpolated on to a uniform lat-lev grid. This nc_files is for MODEL: ' + model + '  FREQ: ' + freq + '  EXPERIMENT: ' + experi + '  REALM: ' + realm + '  VARI: ' + vari
+    rootgrp.history = 'Created ' + time.ctime(time.time()) + '. Original data sourced from DKRZ EUCLIPSE sever.'
     rootgrp.source = 'Cameron Cairns; email: cam.cairns1@gmail.com'
     latitudes.units = 'degrees north'
-    plev.units = 'hPa'
+    if input_plev:
+        plev.units = 'hPa'
+    else:
+        pass
     tmp.units = units_dict[nc_vari]
     times.units = time_units
     times.calendar = time_cal
@@ -184,11 +218,13 @@ def write_nc(input_lat, input_latb, input_plev, input_array,  time_array, time_u
     # Write Data
     latitudes[:] = lat_common
     latitude_bnds[:] = latb_common
-    plev[:] = plev_common
-    print 'Input_array size)', np.shape(input_array)
-    tmp[0:model_size,0:len(plev_common),0:len(lat_common)] = input_array
+    if input_plev:
+        plev[:] = plev_common
+        tmp[0:model_size,0:len(pfull_common),0:len(lat_common)] = input_array
+    else: 
+        tmp[0:model_size,0:len(lat_common)] = input_array
     print 'tmp shape after adding data = ', tmp.shape
-
+    
     rootgrp.close()
 
 def mkdir_p(path):
