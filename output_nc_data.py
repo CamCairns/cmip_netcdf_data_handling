c7ba6bc4caf6a3b5dedf30e45c1e72b49928cd01
CamCairns <cam.cairns1@gmail.com>
2015-05-27 13:20:26 +1000

    program to load interp data of some length finished

--- output_nc_data.py
+++ output_nc_data.py
@@ -1,14 +1,32 @@
-from nc_data import  nc_data as cn
-time_length =  # max_length, min_length or some integer
+import numpy as np
+from nc_data import nc_data as nc
 
-for i1, experi in enumerate(experi_list):
-    for i2, freq in enumerate(freq_list):
-        for i3, realm in enumerate(realm_list):
-            for i4, vari in enumerate(vari_list):
-                for i5, model in enumerate(model_list):
-                    print experi, freq, realm, vari, model
-                    files = get_SPOOKIE_filepath(category,experi, freq, realm, vari, model, mount)
-                    if files:
-                        
-                        #FIELD DATA EXTRACTION AND CONCATENATION    
-                        tmp_array = extract_nc_data(files, nc_vari, tmp_array, zonal_mean=False)
+time_length =  20 # max_length, min_length or some integer
+
+category = 'SPOOKIE_interp'
+experi = 'convoffamip'
+freq = 'mon'
+realm = 'atmos'
+vari =  'ua'
+model_list = ['CanAM4','CNRM-AM6', 'CNRM-CM5','NotInThere']#, 'MIROC5', 'HadGEM2-A', 'MPI-ESM-LR', 'MRI-CGCM3', 'GFDL-HIRAM', 'CESM1-CAM5']
+mount = 'mountpoint3'
+
+files = nc.get_filepath(category,experi, freq, realm, vari, model_list[0], ensemble=None, mount_dir=mount)
+output_array, lat, plev, plev_flag = nc.empty_array_generator(files,time_length, model_dim=len(model_list))
+
+mask_list = []
+for i1, model in enumerate(model_list):
+    print "Loading %s/%s/%s/%s/%s/%s" % (category, experi, freq, realm, vari, model)
+    files = nc.get_filepath(category,experi, freq, realm, vari, model, ensemble=None,mount_dir=mount)
+    if files:
+        model_size = nc.find_model_size(files,vari)
+        tmp_array, lat, plev, plev_flag = nc.empty_array_generator(files, model_size)
+        print np.shape(tmp_array)
+        tmp_array = nc.extract_nc_data(files, vari, tmp_array, zonal_mean=False)
+        output_array[...,i1] = tmp_array[:time_length,...]
+    else:
+        print "The model %s doesn't exist" % model
+        mask_list.append(i1)
+
+keep_mask = set(range(len(model_list))) - set(mask_list)        
+output_array = output_array[...,list(keep_mask)]
