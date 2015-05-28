2bab99d9bf5efef6b2c2c2018b8a19fe985cf130
CamCairns <cam.cairns1@gmail.com>
2015-05-28 16:47:59 +1000

    BugFIX: fetch_nc_data
    Simple problem with optional ensemble arguement
    
    Another bugfix added an optional arguement start_month=1 for completeness

--- output_nc_data.py
+++ output_nc_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import nc_data as nc
 
-def fetch_nc_data(time_length,category,experi, freq, realm, vari, model_list, ensemble=None, mount_dir='mountpoint'):
+def fetch_nc_data(time_length,category,experi, freq, realm, vari, model_list, ensemble=None, mount_dir='mountpoint', start_month=1):
     if time_length in ('max','min'):
         model_size_list = []
         print "The model sizes are:"
@@ -19,17 +19,18 @@ def fetch_nc_data(time_length,category,experi, freq, realm, vari, model_list, en
     else:
         pass
 
-    files = nc.get_filepath(category,experi, freq, realm, vari, model_list[0], ensemble=None, mount_dir=mount_dir)
+    files = nc.get_filepath(category,experi, freq, realm, vari, model_list[0], ensemble, mount_dir=mount_dir)
     output_array, lat, plev, plev_flag = nc.empty_array_generator(files,time_length, model_dim=len(model_list),modulo=12)
     mask_list = []
     for i1, model in enumerate(model_list):
-        files = nc.get_filepath(category,experi, freq, realm, vari, model, ensemble=None,mount_dir=mount_dir)
+        print model
+        files = nc.get_filepath(category,experi, freq, realm, vari, model, ensemble,mount_dir=mount_dir)
         if files:
             model_size = nc.find_model_size(files,vari)
             tmp_array, lat, plev, plev_flag = nc.empty_array_generator(files, model_size)
             tmp_array = nc.extract_nc_data(files, vari, tmp_array, zonal_mean=False)
             time_arg = min(np.size(tmp_array,0),time_length)
-            R = nc.round_time(files, model_size)
+            R = nc.round_time(files, model_size,start_month=start_month)
             output_array[:time_arg-R,...,i1] = tmp_array[R:time_arg,...]
         else:
             print "The model %s doesn't exist" % model
