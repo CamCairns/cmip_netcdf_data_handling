f6b404857194106d21d0809878282b5a62b9a302
CamCairns <cam.cairns1@gmail.com>
2015-06-01 16:31:27 +1000

    New Class
    Can't decide whether to add some OOP

---
+++ ensemble_class.py
@@ -0,0 +1,9 @@
+class Ensemble():
+    def __init__(self,category,experi, freq, realm, vari, mount_dir)
+        self.category = category
+        self.experi = experi
+        self.freq = freq
+        self.realm = realm
+        self.vari = vari
+        self.mount = mount_dir
+        self.data = 
